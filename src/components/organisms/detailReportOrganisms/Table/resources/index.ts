ff1ddbeb22b92e55890d163faa380393da9689f5
mohsen noury <66866297+MNGH-27@users.noreply.github.com>
2025-08-04 21:35:45 +0330

    fix: fix build error

--- src/components/organisms/detailReportOrganisms/Table/resources/index.ts
+++ src/components/organisms/detailReportOrganisms/Table/resources/index.ts
@@ -1,3 +1,2 @@
-import EditOrganModal from './components/EditOrganModal/EditOrganModal'
 import { TABLE_HEAD } from './constants/static-data.constants'
-export { EditOrganModal, TABLE_HEAD }
+export { TABLE_HEAD }
